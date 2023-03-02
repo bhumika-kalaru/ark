03d692aaf0d64ebd562aff74080ee96f85cf221c
Bhumika Kalaru <bhumikakalaru@gmail.com>
2023-03-02 11:27:26 +0530

    changes in location and added constants file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 // late double height, width;
 final Normal = GoogleFonts.montserrat(fontSize: 40);
+
+final Color? white = Colors.white;
 
 // class MyWidget extends StatelessWidget {
 //   const MyWidget({super.key});
--- lib/location.dart
+++ lib/location.dart
@@ -1,6 +1,8 @@
 import 'package:ark/showLocation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'constants.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -32,22 +34,37 @@ class _LocationState extends State<Location> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ElevatedButton(
-        child: Text('hello'),
-        onPressed: (() {
-          _getLocation().then(((value) {
-            lat = '${value.latitude}';
-            long = '${value.longitude}';
-            setState(() {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ShowLocation(latitude: lat, longitude: long)));
-            });
-          }));
-        }),
+    double h = MediaQuery.of(context).size.height,
+        w = (MediaQuery.of(context).size.height);
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: GestureDetector(
+          child: Container(
+              height: h / 15,
+              width: w / 6,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                  child: Text(
+                'Get Location',
+                style: GoogleFonts.openSans(
+                    color: white, fontSize: 20, fontWeight: FontWeight.w600),
+              ))),
+          onTap: (() {
+            _getLocation().then(((value) {
+              lat = '${value.latitude}';
+              long = '${value.longitude}';
+              setState(() {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            ShowLocation(latitude: lat, longitude: long)));
+              });
+            }));
+          }),
+        ),
       ),
     );
   }
