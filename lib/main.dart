8c6b35ab8d75af1ea9ee85d0504bc294c39f1566
Bhumika Kalaru <bhumikakalaru@gmail.com>
2023-03-07 17:22:01 +0530

    firebase done checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:ark/location.dart';
 import 'package:ark/pickFile.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
