edf8323c5f5bc491d8d5657da2d928bcf44f3149
Bhumika Kalaru <bhumikakalaru@gmail.com>
2023-04-07 19:16:40 +0530

    read the uploaded file

--- lib/Screens/mainScreen.dart
+++ lib/Screens/mainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:ark/Login/forgotPassword.dart';
 import 'package:ark/Screens/profile.dart';
 import 'package:ark/Widgets/eventButton.dart';
 import 'package:ark/Widgets/location.dart';
@@ -7,6 +8,36 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import '../constants.dart';
 import 'package:flutter/material.dart';
+import 'textExtracter.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:pdf_render/pdf_render.dart';
+
+// Future<String?> _extractText() async {
+//   final result = await FilePicker.platform.pickFiles(
+//     type: FileType.custom,
+//     allowedExtensions: ['pdf'],
+//   );
+
+//   if (result == null) {
+//     // User cancelled the picker
+//     return null;
+//   }
+
+//   final file = File(result.files.single.path!);
+//   final doc = await PdfDocument.openFile(file.path);
+//   final pageCount = doc.pageCount;
+//   String text = "";
+
+//   for (int i = 0; i < pageCount; i++) {
+//     final page = await doc.getPage(i + 1);
+//     final pageText = page.toString();
+//     text += pageText;
+//     // await page.close();
+//   }
+
+//   return text;
+// }
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -16,8 +47,11 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late final files, path, file, fileText = "hello";
+  TextEditingController txt = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    // final Future<String?> s = _extractText();
     return Scaffold(
       drawer: Drawer(
           child: ListView(children: [
@@ -27,6 +61,14 @@ class _MainScreenState extends State<MainScreen> {
           onTap: () {
             Navigator.push(
                 context, MaterialPageRoute(builder: (context) => profile()));
+          },
+        ),
+        ListTile(
+          tileColor: white,
+          title: Center(child: Text('Change Password')),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ForgotPassword()));
           },
         ),
       ])),
@@ -46,7 +88,42 @@ class _MainScreenState extends State<MainScreen> {
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Location(), FilePick(), EventButton()],
+            children: [
+              Location(),
+              FilePick(),
+              EventButton(),
+              GestureDetector(
+                  onTap: (() {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FlutterDemo(
+                                  storage: CounterStorage(),
+                                )));
+                  }),
+                  child: TextButton(
+                      onPressed: () {
+                        print('read succesfully');
+                        setState(() async {
+                          FilePickerResult? result = await FilePicker.platform
+                              .pickFiles(allowMultiple: false);
+
+                          if (result == null) {
+                            return;
+                          }
+                          files = result.files;
+                          path = files.first.path.toString();
+                          file = File(path);
+                          final fileText = await file.readAsString();
+                          txt.text = fileText;
+                          // print(fileText);
+                        });
+                      },
+                      child: Text('read'))
+                  // Text('s.toString()'),
+                  )
+              // _extractText();
+            ],
           ),
         ),
       ),
